d723a6fd75252eae7886d3b733cd786835833df1
AmarinoLama <amarinolama@gmail.com>
2024-01-11 10:55:53 +0100

    refactor (yatzy.py) : refactorizar la funcion four_of_a_kind

--- yatzy.py
+++ yatzy.py
@@ -177,16 +177,16 @@ class Yatzy:
         return 0
     
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+    def four_of_a_kind(*dices):
+        
+        '''
+        adashdahsohd
+        '''
+        
+        dices_sorted = list(sorted(dices, reverse=True))
+        for number in dices_sorted:
+            if dices.count(number) >= 4:
+                return number*4
         return 0
     
 
