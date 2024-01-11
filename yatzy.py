e0234f155f592a939cf30c1ca91b34116fb42ed6
AmarinoLama <amarinolama@gmail.com>
2024-01-11 11:22:02 +0100

    refactor (yatzy.py) : refactorizar funcion  smallStraight

--- yatzy.py
+++ yatzy.py
@@ -205,21 +205,18 @@ class Yatzy:
     
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+    def smallStraight(*dices):
+        
+        '''
+        asdaidjadj
+        '''
+        dices_sorted = sorted(dices)
+        
+        if dices_sorted == [1,2,3,4,5]:
             return 15
-        return 0
-    
+        else:
+            return 0
+
 
     @staticmethod
     def largeStraight( d1,  d2,  d3,  d4,  d5):
